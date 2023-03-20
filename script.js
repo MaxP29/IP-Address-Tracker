818ba7b6bc171327adc4097b82e64de6f9b95b57
MaxP29 <113713921+MaxP29@users.noreply.github.com>
2023-03-20 15:43:50 +0000

    Updated for Screen Sizes

--- script.js
+++ script.js
@@ -6,82 +6,85 @@ const Location = document.querySelector(`.LOCATION`);
 const timezone = document.querySelector(`.TMZ`);
 const ISPV = document.querySelector(`.ISPV`);
 
+// Map Icon
+const myIcon = L.icon({
+  iconUrl: "/images/icon-location.svg",
+  iconSize: [46, 56],
+});
+
+// Functions
+const updateValue = function (data) {
+  Location.textContent = `${data.location.localityName}, ${data.location.principalSubdivision}`;
+  timezone.textContent = data.location.timeZone.displayName.split(` `)[0];
+  ISPV.textContent = data.network.carriers[0].organisation;
+  IPA.textContent = data.ip;
+};
+
+const generateMap = function (map) {
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+};
+
+const generateIcon = function (map, coords, myIcon) {
+  L.marker(coords, { icon: myIcon }).addTo(map);
+};
+
+// Fetch Requests & Event Listener
+
 fetch(
   `https://api.bigdatacloud.net/data/ip-geolocation?localityLanguage=en&key=bdc_64b55ab820604619a4ef332d798df262`
 )
   .then((response) => response.json())
   .then((data) => {
-    // Adjusting The Values Based Off Current User Location
-    Location.textContent = `${data.location.localityName}, ${data.location.principalSubdivision}`;
-    timezone.textContent = data.location.timeZone.displayName.split(` `)[0];
-    ISPV.textContent = data.network.carriers[0].organisation;
-    IPA.textContent = data.ip;
-
+    // Collects Data
     const latitude = data.location.latitude;
     const longitude = data.location.longitude;
-
     let coords = [latitude, longitude];
 
-    // Sets Map With Current Coordinates & Remove Zoom Control Buttons
     let map = L.map("map", { zoomControl: false }).setView(coords, 17);
 
-    const myIcon = L.icon({
-      iconUrl: "/images/icon-location.svg",
-      iconSize: [46, 56],
-    });
-
-    // Creates Map
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
-
-    // Adds Marker to Map Location
-    L.marker(coords, { icon: myIcon }).addTo(map);
+    // Updates UI
+    updateValue(data);
+    generateMap(map);
+    generateIcon(map, coords, myIcon);
 
     // Event Listener Upon Search
-    button.addEventListener(`click`, function (e) {
-      e.preventDefault();
+    button.addEventListener(`click`, function () {
       const request = search.value;
 
-      if (!request == "") {
-        fetch(
-          `https://api-bdc.net/data/ip-geolocation?ip=${request}&localityLanguage=en&key=bdc_64b55ab820604619a4ef332d798df262`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
+      if (request === "" || !request.includes(`.`))
+        throw new Error(`Invalid Format, try again!`);
 
-            // Adjusting The Values Based Off Current User Location
-            Location.textContent = `${data.location.localityName}, ${data.location.principalSubdivision}`;
-            timezone.textContent =
-              data.location.timeZone.displayName.split(` `)[0];
-            ISPV.textContent = data.network.carriers[0].organisation;
-            IPA.textContent = data.ip;
+      fetch(
+        `https://api-bdc.net/data/ip-geolocation?ip=${request}&localityLanguage=en&key=bdc_64b55ab820604619a4ef332d798df262`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          if (!data.isReachableGlobally)
+            throw new Error(`That Ip Address Doesn't Exist, please try again!`);
 
-            const latitude = data.location.latitude;
-            const longitude = data.location.longitude;
+          updateValue(data);
 
-            let coords = [latitude, longitude];
+          const latitude = data.location.latitude;
+          const longitude = data.location.longitude;
 
-            // Deleting Old Map and Loading New Map Location
-            map.remove();
-            map = L.map("map").setView(coords, 17);
+          let coords = [latitude, longitude];
 
-            L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-              maxZoom: 19,
-              attribution:
-                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            }).addTo(map);
+          // Deleting Old Map
+          map.remove();
 
-            // Adds Marker to Map Location
-            L.marker(coords, { icon: myIcon }).addTo(map);
-          });
-      } else {
-        return;
-      }
+          // Set Map to Map Id - Html
+          map = L.map("map", { zoomControl: false }).setView(coords, 17);
+
+          // Creates new Map
+          generateMap(map);
+          generateIcon(map, coords, myIcon);
+        })
+        .catch((error) => console.error(error));
     });
   });
-
-// REMINDER - ADD Error Message If Request isn't correct - Fix Spacing and Text Alignment Issues, Then Fix Mobile Media Queries
